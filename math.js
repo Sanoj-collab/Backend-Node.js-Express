f385ca48f3bdb1e7ecaadcc736765d427bd83b2f
Sanoj <realsanoj9875@gmail.com>
2026-03-16 13:22:59 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,4 @@
+const script = require("./script"); // require() - a built-in function o include modules that exist in seperate files.
+
+console.log(script.sum(2, 2));
+console.log(script.pi)
---
+++ script.js
@@ -0,0 +1,37 @@
+// let n = 5;
+// for(let i = 1; i <= n; i++){
+//     console.log("Hello , world");
+// }
+// console.log("Byee!!");
+
+// console.log("Process: The object that is currently beiongb executed in node.js enviroment and it contains the information about the cureent preocess .");
+
+// PROCESS.ARGV : return an array containing the command line arguments passed when the node.js process as launced.
+
+// let args = process.argv;
+// for(let i = 2; i < args.length; i++){
+//     console.log("Hello to ", args[i]);
+// }
+
+// const sum = (a, b) => a+b;
+// const product = (a,b) => a*b;
+// const divide = (a,b) => a/b;
+// const g = 9;
+// const pi = 3.14;
+
+// let obj = {
+//     sum : sum,
+//     product : product,
+//     divide: divide,
+//     g: g,
+//     pi: pi,
+// };
+
+// module.exports = obj;
+
+// module. exports - a special object which is included in every JS file in node.js by default. We can use module.exports to export functions, objects or primitive values from a given file so they can be used in other files using require() function.
+//module.exports = "hello";
+
+// --------------------
+
+
